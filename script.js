e77456f002b6c48f5dcc478ceeb4ff966332c137
@ascndia <119561063+ascndia@users.noreply.github.com>
2023-06-01 17:26:37 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -78,9 +78,13 @@ let menus = [
             ]
     }
 ]
-
+let is = false;
 let orders = [];
-
+const orderheader = document.querySelector('#order-header');
+const expand = document.querySelector('#expand');
+const ordercontainer = document.querySelector('#ordercontainer');
+const orderlist = document.querySelector('#orderlist');
+const deleteorder = document.querySelector('#delete-order'); 
 const menulist = document.querySelector("main");
 const orderbottom = document.querySelector("order");
 const categories = document.querySelector('#categories');
@@ -93,6 +97,7 @@ function RenderList(id = 0){
         list.classList.add("list");
         
         const n = document.createElement('span');
+        n.classList.add('list-name');
         n.textContent = h.name;
         
         const o = document.createElement('div');
@@ -132,6 +137,7 @@ function RenderList(id = 0){
         
         const n = document.createElement('span');
         n.textContent = b.name;
+        n.classList.add('list-name');
         
         const o = document.createElement('div');
         o.classList.add('list-counter');
@@ -165,6 +171,7 @@ function RenderList(id = 0){
 
 RenderList();
 RenderCategories();
+//RenderOrderTop();
 RenderOrderBottom();
 
 menulist.addEventListener('click', e => {
@@ -186,12 +193,15 @@ menulist.addEventListener('click', e => {
             const b = e.target.parentElement;
         let new_order = {};
         new_order.id = b.dataset.id;
+        new_order.price = e.target.textContent;
+        new_order.name = b.querySelector('.list-name').textContent;        
         new_order.orderid = new Date().getTime();
         new_order.categories = b.dataset.category;
         new_order.count = Number(b.querySelector('input').value);
         CheckDuplicateOrder(new_order)
-        RenderOrderBottom()
-        
+        //RenderOrderTop();
+        RenderOrderBottom();
+        RenderOrderList();        
     }
 })
 
@@ -228,9 +238,9 @@ function CheckDuplicateOrder(Order){
         orders = orders.filter(a => {                  
             return a.orderid !== found.orderid 
         });              
-        orders.unshift(Order);        
+        orders.push(Order);        
     } else {
-        orders.unshift(Order);
+        orders.push(Order);
     }   
 }
 
@@ -244,7 +254,7 @@ function RenderOrderBottom(){
         let sum = 0;
         let totalprice = 0;        
         orders.forEach(e => {
-            sum += e.count;
+            sum += parseInt(e.count);
             const selected_menu = menus.find(k => {
                 return k.name == e.categories 
                 }).menu.find(s => {
@@ -253,9 +263,170 @@ function RenderOrderBottom(){
             totalprice += selected_menu.price * e.count;
         })
         const rp = ' Rp'
-        items.textContent = sum + ' Items'
+        items.textContent = parseInt(sum).toString() + ' Items'
         price.textContent = 'Total: ' + totalprice.toLocaleString() + rp;
         
     }
 }
+function RenderOrderList(){
+    orderlist.textContent = "";
+    if(!is){
+        return;
+    }
+    const aa = 'Rp ';
+    if(orders.length == 0){
+        return;
+    } else {
+    orders.forEach(order => {
+        const a = document.createElement('div');
+        a.classList.add('olist');
+        const b = document.createElement('div');
+        b.classList.add('i');
+        const c = document.createElement('span');
+        c.textContent = order.name;
+        const d = document.createElement('span');
+        d.classList.add('mini');
+        d.textContent = 'Price: ' + aa + parseInt(order.price).toLocaleString();
+        const e = document.createElement('span');
+        e.classList.add('mini');
+        e.textContent = 'Total Price: ' + aa + parseInt(order.price * order.count).toLocaleString();
+        const f = document.createElement('div');
+        f.classList.add('c');
+        const g = document.createElement('button');
+        g.classList.add('min');
+        g.textContent = '-';
+        const h = document.createElement('button');
+        h.classList.add('plus');
+        h.textContent = '+';
+        const i = document.createElement('input');
+        i.setAttribute('type','number');
+        i.value = order.count;
+        i.classList.add('lololo');
+        const j = document.createElement('button');
+        j.classList.add('rmv');   
+        j.textContent = 'Remove Order';      
         
+        f.appendChild(g);
+        f.appendChild(i);
+        f.appendChild(h);
+              
+        b.appendChild(c);
+        b.appendChild(d);
+        b.appendChild(e);
+        
+        a.appendChild(b);
+        a.appendChild(f);
+        a.appendChild(j);
+        a.dataset.orderid = order.orderid;        
+        orderlist.appendChild(a);        
+    }) 
+    }
+}
+function DeleteOrder(){
+    orders = [];
+    RenderOrderList();
+    RenderOrderBottom();
+}
+deleteorder.addEventListener('click',DeleteOrder);
+orderlist.addEventListener('click',e => {
+    if(e.target.classList.contains('min')){
+        if(e.target.parentElement.querySelector('input').value <= 1){
+            return;
+        }
+        const u = e.target.parentElement.parentElement;        
+        orders.map(order => {
+            if(order.orderid == u.dataset.orderid){
+                order.count--;
+            }
+        })
+        //RenderOrderTop();
+        RenderOrderBottom();
+        RenderOrderList();
+    } else if(e.target.classList.contains('plus')){        
+        const u = e.target.parentElement.parentElement;        
+        orders.map(order => {
+            if(order.orderid == u.dataset.orderid){
+                order.count++;
+            }
+        })
+        //RenderOrderTop();
+        RenderOrderBottom();
+        RenderOrderList();
+    } else if(e.target.classList.contains('rmv')){
+        const u = e.target.parentElement;                  
+        orders = orders.filter(order => {               
+            return order.orderid != u.dataset.orderid;
+        })
+        RenderOrderBottom();
+        RenderOrderList();
+    }
+})
+orderlist.addEventListener('change',e => {
+    if(e.target.classList.contains('lololo')){
+        const a = e.target.parentElement.parentElement;
+        orders.map(order => {
+            if(order.orderid == a.dataset.orderid){
+            
+                order.count = e.target.value;
+            }
+        })
+        RenderOrderList();
+        RenderOrderBottom();
+    }
+})
+
+expand.addEventListener('click',() => {
+    if(ordercontainer.classList.contains('expanded')){
+           
+      
+       ordercontainer.classList.toggle('expanded'); 
+       is = false;
+        //RenderOrderTop();
+        orderheader.classList.add('hidden')       
+        RenderOrderList();
+        RenderOrderBottom();
+        expand.textContent = 'Expand';
+                
+        
+    } else {
+        ordercontainer.classList.toggle('expanded');
+        is = true;
+        //RenderOrderTop();       
+        RenderOrderList();
+        RenderOrderBottom();
+        orderheader.classList.remove('hidden');
+        expand.textContent = 'Shrink';        
+    }
+    
+})
+/*
+function RenderOrderTop(){
+    if(!is){
+        return;
+    } else {
+    
+    orderheader
+        
+        orderheader.textContent = "";
+        const a = document.createElement('div');
+        a.setAttribute('id','order-header');
+        const b = document.createElement('span');
+        b.textContent = 'Ruruw Cafe';
+        const c = document.createElement('button');
+        c.setAttribute('id','delete-order');
+        c.textContent = 'Delete';
+        a.appendChild(b);
+        a.appendChild(c);
+        orderheader.appendChild(a);
+        
+    }
+}
+*/
+/*
+
+    <div id="order-header">
+                <span>Ruruw Cafe</span>
+                <button id="delete-order">Delete</button>
+            </div>
+
+*/
